c981577baabc230caacea80519deda6625781ee3
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-01-07 14:59:31 +0700

    Adding Header

---
+++ src/elements/Button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import propTypes from "prop-types";
+
+export default function Button(props) {
+    const className = ["button"];
+    if(props.isPrimary) className.push("button-primary");
+    if(props.isWideMobile) className.push("button-wide-mobile");
+    if(props.isSmall) className.push("button-sm");
+
+    return <button className={className.join(" ")}>{props.children}</button>
+}
+
+Button.PropTypes = {
+    isPrimary: propTypes.bool,
+    isWideMobile: propTypes.bool,
+    isSmall: propTypes.bool
+};
--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import Btn from '../elements/Buttons'
+import Header from "parts/Header"
 
 export default function Homepage() {
     return (
         <div>
-            <Btn>Sample</Btn>
+            <Header></Header>
         </div>
     )
 }
